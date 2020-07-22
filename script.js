d40d52ad1a64d433855e0105cb29b8358fae9c0d
tariktabakovic <tariktab97@gmail.com>
2020-07-22 16:16:58 -0400

    create breatheAnimation function and calls it

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const container = document.querySelector('.container');
 const text = document.querySelector('#text');
 
+breatheAnimation()
+
 const totalTime = 7500;
 const breatheTime = (totalTime / 5 ) * 2;
 const holdTime = (totalTime / 5);
+
+function breatheAnimation(){
+    text.innerHTML = 'Breathe in!'
+    container.className = 'container grow'
+
+    setTimeout(()=> {
+        text.innerText = 'Hold'
+
+        setTimeout(()=>{
+            text.innerText = 'Breathe Out!'
+            container.className = 'container shrink'
+        }, holdTime)
+    }, breatheTime)
+}
