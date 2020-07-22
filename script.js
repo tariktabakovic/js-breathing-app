10e397016fc9d224e20ec03515fd411676f7df84
tariktabakovic <tariktab97@gmail.com>
2020-07-22 16:13:38 -0400

    sets time intervals for breathing in/out

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const container = document.querySelector('.container');
 const text = document.querySelector('#text');
 
+const totalTime = 7500;
+const breatheTime = (totalTime / 5 ) * 2;
+const holdTime = (totalTime / 5);
